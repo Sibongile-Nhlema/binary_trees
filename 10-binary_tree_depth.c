58133dd2fc3ff85eb2844776b191f26e3374f4c3
Sibongile <sibongile.nhlema@gmail.com>
2024-01-31 15:51:18 +0000

    fixed the betty issues

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -4,6 +4,7 @@
  * binary_tree_depth - function that measures the depth
  * of a node in a binary tree
  * @tree: pointer to the node to measure the depth
+ * Return: the depth or NULL
  */
 
 size_t binary_tree_depth(const binary_tree_t *tree)
