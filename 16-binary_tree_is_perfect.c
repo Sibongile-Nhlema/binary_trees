e6349ab771d0cb64e6001cb84b3bd6b1b85d9141
Sibongile <sibongile.nhlema@gmail.com>
2024-02-01 10:08:48 +0000

    completed task 16: checks if the tree is perfect

---
+++ 16-binary_tree_is_perfect.c
@@ -0,0 +1,17 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_perfect - function that checks if a binary tree is perfect
+ * @tree: pointer to the root node of the tree to check
+ * Return: 1 if the tree is full, otherwise 0
+ */
+int binary_tree_is_prefect(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+
+	/*check if node has two children*/
+	if (tree->left != NULL && tree->right != NULL)
+		return (binary_tree_is_perfect(tree->left) && binary_tree_is_perfect(tree->right));
+	return (0);
+}
